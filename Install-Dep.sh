7626dfebfb874bfd22f3b038c129905c370492ce
Ubuntu <ubuntu@ubuntu-HP-Pavilion-g7-Notebook-PC.(none)>
2012-09-16 14:15:26 -0400

    Dependencies for building on Ubuntu

---
+++ Install-Dep.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt-get install build-essential  libsdl1.2debian libsdl1.2-dev libsdl-image1.2 libsdl-image1.2-dev libsdl-mixer1.2 libsdl-mixer1.2-dev libsdl-net1.2 libsdl-net1.2-dev  mesa-common-dev libglu1-mesa-dev
+read
